afc69695db2acb1d3e6f384359de1c5fdd3a23ad
shestmintsev-kirill <1kirillmihail1@gmail.com>
2021-01-30 23:05:31 +0300

    test 3D

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,315 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="section-outer">
+    <!-- Header -->
+    <section class="header">
+      <details class="mobil">
+        <summary>Контакты</summary>
+        <ul>
+          <select name="City">
+            <option>Санкт-Петербург</option>
+          </select>
+          <li>+7 (812) 613-58-38</li>
+          <li>Пн–пт 10:00–19:00, сб-вс 10:00–17:00</li>
+          <li><a href="#">Напишите нам</a></li>
+          <li><a href="#">Свяжитесь с директором</a></li>
+          <img src="@/assets/img/skype.svg" alt="skype" />
+          <img src="@/assets/img/telegram.svg" alt="telegram" />
+          <img src="@/assets/img/viber.svg" alt="viber" />
+        </ul>
+      </details>
+      <section class="section-head">
+        <div class="section-inner">
+          <header class="section-head-content">
+            <div class="section-head-content-left">
+              <ul>
+                <select name="City">
+                  <option>Санкт-Петербург</option>
+                </select>
+                <li>+7 (812) 613-58-38</li>
+                <li>Пн–пт 10:00–19:00, сб-вс 10:00–17:00</li>
+              </ul>
+            </div>
+            <div class="section-head-content-right">
+              <ul>
+                <li><a href="#">Напишите нам</a></li>
+                <li><a href="#">Свяжитесь с директором</a></li>
+                <img src="@/assets/img/skype.svg" alt="skype" />
+                <img src="@/assets/img/telegram.svg" alt="telegram" />
+                <img src="@/assets/img/viber.svg" alt="viber" />
+              </ul>
+            </div>
+          </header>
+        </div>
+      </section>
+      <section class="section-header">
+        <nav class="section-header-main">
+          <div class="section-inner">
+            <div class="section-header-main_info">
+              <p class="italic">
+                <img src="@/assets/img/logo.svg" alt="logo" />
+                <span
+                  >Ваш эксперт <br />
+                  на рынке <span class="color-text">3D</span> -техники</span
+                >
+              </p>
+              <div class="yandex">
+                <img src="@/assets/img/yandex.svg" alt="yandex" />
+              </div>
+              <form>
+                <input
+                  type="search"
+                  placeholder="Введите запрос, например, Formlabs Form 2"
+                />
+                <button class="btn" type="submit">Найти</button>
+              </form>
+              <p>
+                <img src="@/assets/img/in.svg" alt="in" />
+                <span><a href="#">Войти</a></span>
+              </p>
+              <div class="mobil-bin">
+                <button class="bin">
+                  <img src="@/assets/img/bin.svg" alt="bin" /><span
+                    >Корзина пуста</span
+                  >
+                </button>
+              </div>
+            </div>
+          </div>
+          <details class="mobil-links">
+            <summary>Меню</summary>
+            <ul class="mobil-menu">
+              <ul>
+                <li><a href="#">Акции</a></li>
+                <li><a href="#">О нас</a></li>
+              </ul>
+              <ul>
+                <li><a href="#">Мероприятия</a></li>
+                <li><a href="#">Оплата и доставка</a></li>
+              </ul>
+              <ul>
+                <li><a href="#">Что с моим заказом?</a></li>
+                <li><a href="#">Гос. закупки</a></li>
+              </ul>
+              <ul>
+                <li><a href="#">Мастер-классы</a></li>
+                <li><a href="#">Контакты</a></li>
+              </ul>
+            </ul>
+          </details>
+          <div class="section-header-main_links">
+            <div class="section-inner">
+              <ul>
+                <li><a href="#">Акции</a></li>
+                <li><a href="#">О нас</a></li>
+                <li><a href="#">Мероприятия</a></li>
+                <li><a href="#">Оплата и доставка</a></li>
+                <li><a href="#">Что с моим заказом?</a></li>
+                <li><a href="#">Гос. закупки</a></li>
+                <li><a href="#">Блог</a></li>
+                <li><a href="#">Мастер-классы</a></li>
+                <li><a href="#">Контакты</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <details class="mobil-katalog">
+          <summary>Каталог</summary>
+          <ul class="mobil-menu">
+            <ul>
+              <select>
+                <option>3D-принтеры</option>
+              </select>
+              <select>
+                <option>3D-сканеры</option>
+              </select>
+            </ul>
+            <ul>
+              <select>
+                <option>Станки</option>
+              </select>
+              <select>
+                <option>Роботы</option>
+              </select>
+            </ul>
+            <ul>
+              <select>
+                <option>Образование</option>
+              </select>
+              <select>
+                <option>Стоматологам</option>
+              </select>
+            </ul>
+            <ul>
+              <select>
+                <option>Гаджеты</option>
+              </select>
+              <select>
+                <option>Материалы</option>
+              </select>
+            </ul>
+            <ul>
+              <select>
+                <option>ПО</option>
+              </select>
+              <select>
+                <option>Решения</option>
+              </select>
+            </ul>
+            <ul>
+              <select>
+                <option>Учебный центр</option>
+              </select>
+              <select>
+                <option>Услуги</option>
+              </select>
+            </ul>
+          </ul>
+        </details>
+        <div class="section-header_footer">
+          <div class="section-inner">
+            <ul>
+              <select>
+                <option>3D-принтеры</option>
+              </select>
+              <select>
+                <option>3D-сканеры</option>
+              </select>
+              <select>
+                <option>Станки</option>
+              </select>
+              <select>
+                <option>Роботы</option>
+              </select>
+              <select>
+                <option>Образование</option>
+              </select>
+              <select>
+                <option>Стоматологам</option>
+              </select>
+              <select>
+                <option>Гаджеты</option>
+              </select>
+              <select>
+                <option>Материалы</option>
+              </select>
+              <select>
+                <option>ПО</option>
+              </select>
+              <select>
+                <option>Решения</option>
+              </select>
+              <select>
+                <option>Учебный центр</option>
+              </select>
+              <select>
+                <option>Услуги</option>
+              </select>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </section>
+    <!-- Header -->
+
+    <!-- Body -->
+    <section class="section-inner section-body">
+      <div class="section-body_photo">
+        <img src="@/assets/img/team.png" alt="team" />
+      </div>
+      <div class="section-body-content">
+        <div class="section-body-content_work">
+          <div class="section-body-content_work__title">
+            <p>Твоя работа в крупнейшем интеграторе цифрового производства</p>
+          </div>
+          <div class="section-body-content_work__submit">
+            <button>Отправить резюме</button>
+          </div>
+          <div class="section-body-content_work__subdescr">
+            <p>
+              Нажимая кнопку, вы соглашаетесь на обработку персональных данных.
+            </p>
+          </div>
+        </div>
+        <div class="section-body-content_description">
+          <p>
+            Top 3D Shop – лидер по продажам 3D-оборудования в России и странах
+            СНГ. В 2018 мы стали лучшим интегратором 3D оборудования по
+            аддитивным технологиям и цифровому производству по версии
+            <a href="#">3D Print Awards</a>. <br /><br />
+            За 6 лет работы более 10 000 клиентов по всей России и СНГ выбрали
+            Top 3D Shop. В 2018 наш оборот составил 600 млн. рублей, в компании
+            работает более 100 сотрудников. <br /><br />
+            Новая цель: 1 млрд торгового оборота в этом году и выход на
+            международные рынки. Если вы — целеустремленный специалист, готовый
+            учиться новым навыкам и учить других, мыслите не просто как
+            исполнитель, а как владелец своего направления, то нам по пути!
+          </p>
+        </div>
+      </div>
+      <div class="section-body-footer">
+        <div class="section-body-footer-view">
+          <p>Лучше один раз увидеть:</p>
+          <div class="hr"></div>
+        </div>
+        <div class="section-body-footer-links">
+          <ul>
+            <li>
+              <a href="#"> Как устроена наша компания изнутри</a
+              ><img src="@/assets/img/vector.svg" alt="vector" />
+            </li>
+            <li>
+              <a href="#">Как создавалась компания - необычная история</a
+              ><img src="@/assets/img/vector.svg" alt="vector" />
+            </li>
+            <li>
+              <a href="#">Читай о нас на 3D Today</a
+              ><img src="@/assets/img/vector.svg" alt="vector" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <!-- Body -->
+    <!-- Footer -->
+    <section class="section-footer">
+      <div class="section-inner">
+        <div class="section-footer-title">
+          <div class="section-footer-title_description">Видео о нас</div>
+          <div class="section-footer-title_slider">
+            <button class="btn">
+              <img class="image" src="@/assets/img/slider.svg" alt="slider" />
+            </button>
+            <button class="btn btn-2">
+              <img class="image" src="@/assets/img/slider.svg" alt="slider" />
+            </button>
+          </div>
+        </div>
+        <div class="section-footer-video">
+          <div class="section-footer-video_first">
+            <img src="@/assets/img/video1.png" alt="video" />
+          </div>
+          <div class="section-footer-video_last">
+            <img src="@/assets/img/video2.png" alt="video" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Footer -->
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+  name: "App"
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "@/assets/scss/style.scss";
+@import "normalize-scss";
+@include normalize();
+
+* {
+  box-sizing: border-box;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "normalize-scss";
 
 Vue.config.productionTip = false;
 
